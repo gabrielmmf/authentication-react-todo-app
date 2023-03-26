f720ff79b14a639fd61dee708e8eb4e4ed6cae4d
Gabriel Fialho <gabrielmmfialho@hotmail.com>
2023-03-25 23:53:03 -0300

    Connected to ElephantSQL database server

--- server/db.js
+++ server/db.js
@@ -3,11 +3,11 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'admin@123',
-    host: 'localhost',
-    port: 5432,
-    database: 'todoapp'
+    user: process.env.DB_UNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DBPORT,
+    database: process.env.DBNAME
 })
 
 module.exports = pool
